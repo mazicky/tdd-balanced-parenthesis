6546df0d859e4312bcaa46318de9e849ef2cefba
Maz <mazicky@gmail.com>
2016-06-08 23:02:17 +0900

    Pass ASAP

--- src/utils/judge.js
+++ src/utils/judge.js
@@ -1,6 +1,19 @@
 export default function parenthesis(string) {
-  if (string.length === 2) {
+  let stackSize = 0
+  for (let i = 0; i < string.length; ++i) {
+    const char = string.charAt(i)
+    if (char === '(') {
+      stackSize++
+    } else if (char === ')') {
+      stackSize--
+    }
+    if (stackSize < 0)
+      return false
+  }
+
+  if (stackSize === 0) {
     return true
   }
+
   return false
 }
